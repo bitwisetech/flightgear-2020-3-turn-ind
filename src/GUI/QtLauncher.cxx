4dce306247c44ef305fdeeea8d88f85b41d6c9c4
James Turner <james@flightgear.org>
2021-09-28 14:33:25 +0100

    Launcher: improve local acft rescan behaviour

--- src/GUI/QtLauncher.cxx
+++ src/GUI/QtLauncher.cxx
@@ -706,10 +706,6 @@ bool runLauncherDialog()
     }
 
     int appResult = qApp->exec();
-    if (appResult <= 0) {
-        return false; // quit
-    }
-
     // avoid crashes / NavCache races if the loader is still running after
     // the launcher exits
     if (naturalEarthLoader) {
@@ -724,6 +720,10 @@ bool runLauncherDialog()
     globals->clear_fg_scenery();
     globals->get_locale()->clear();
 
+    if (appResult <= 0) {
+        return false; // quit
+    }
+    
     return true;
 }
 
